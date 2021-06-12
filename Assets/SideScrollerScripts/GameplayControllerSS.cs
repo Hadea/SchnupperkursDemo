98f845c13c1a5c756c42dfd471d8b17a68799967
Lutz <lambda2@gmx.at>
2021-06-12 12:32:19 +0200

    coin collection

--- Assets/SideScrollerScripts/GameplayControllerSS.cs
+++ Assets/SideScrollerScripts/GameplayControllerSS.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayControllerSS : MonoBehaviour
 {
     [SerializeField] private Canvas PauseMenuCanvas;
+    [SerializeField] private Text UICoinDisplay;
+
+    public static GameplayControllerSS controller;
+    private int collectedCoins = 0;
+
+    private void Awake()
+    {
+        controller = this;
+    }
+
+    public void AddCoin(int CoinsToAdd)
+    {
+        collectedCoins += CoinsToAdd;
+        UICoinDisplay.text = collectedCoins.ToString();
+    }
 
     void Update()
     {
